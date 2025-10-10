5628dfc2873c02774df20d91ad7bc83aabe3d507
Felipe Leite <lipi4shared@hotmail.com>
2025-10-10 02:26:18 -0300

    Definição manual de duplas.

--- src/app/(private)/tournament/[id]/manual/page.tsx
+++ src/app/(private)/tournament/[id]/manual/page.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import { SubmitHandler, useFieldArray, useForm } from "react-hook-form"
-import { Check, ChevronsUpDown } from "lucide-react"
+import { Check, ChevronsUpDown, Eraser } from "lucide-react"
 import { useParams } from "next/navigation"
 import { fetchTournamentById } from "@/lib/api"
 import { useQuery } from "@tanstack/react-query"
@@ -73,8 +73,8 @@ export default function Manual() {
 		label: subs.person.name
 	}))
 
-	console.log('participants', participants)
-	console.log(watch(`pairs.${3}.name`))
+	const includedNames = getValues('pairs').map(item => item.name)
+	const isReady = includedNames.every(Boolean)
 
 	return (
 		<div className="flex flex-col gap-6">
@@ -82,65 +82,93 @@ export default function Manual() {
 
 			<p className="text-sm">Preencha todos as vagas com os atletas correspondentes.</p>
 
-			<form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
-				{fields.map((field, index) => {
-					const currentValue = getValues(`pairs.${index}.name`)
+			{participants && (
+				<form onSubmit={handleSubmit(onSubmit)} className="grid grid-cols-2 gap-4 w-[36rem] items-end">
+					{fields.map((field, index) => {
+						const currentValue = getValues(`pairs.${index}.name`)
+						const hasTitle = index % 2 === 0
 
-					return (
-						<div key={field.id} className="grid grid-cols-2 items-center">
-							{participants && (
-								<Popover>
-									<PopoverTrigger asChild>
-										<Button
-											variant="outline"
-											role="combobox"
-											aria-expanded={open}
-											className="w-[200px] justify-between"
+						return (
+							<div className="flex flex-col gap-2">
+								{hasTitle && (
+									<span className="font-semibold text-md col-span-2">
+										<h2>
+											Dupla {index / 2 + 1}
+										</h2>
+									</span>
+								)}
+
+								<div key={field.id} className="grid grid-cols-[14rem_1rem] gap-2">
+									{/* Necessário para funcionarem os selects */}
+									<span className="hidden">{watch(`pairs.${index}.name`)}</span>
+
+									<Popover>
+										<PopoverTrigger asChild>
+											<Button
+												variant="outline"
+												role="combobox"
+												aria-expanded={open}
+												className="w-56 justify-between self-end"
+											>
+												{currentValue ? 
+													participants.find(participant => participant.value === currentValue.toLowerCase())?.label : 
+													'Selecione...'}
+												<ChevronsUpDown className="opacity-50" />
+											</Button>
+										</PopoverTrigger>
+
+										<PopoverContent className="w-56 p-0">
+											<Command>
+												<CommandInput placeholder="Digite o nome..." className="h-9" />
+												<CommandList>
+													<CommandEmpty>Atleta não encontrado.</CommandEmpty>
+
+													<CommandGroup>
+														{participants.map(participant => (
+															<CommandItem
+																key={participant.value}
+																value={participant.value}
+																onSelect={value => {
+																	setValue(`pairs.${index}.name`, value)
+																}}
+																disabled={includedNames.includes(participant.value)}
+															>
+																{participant.label}
+
+																<Check
+																	className={cn(
+																		"ml-auto",
+																		currentValue === participant.value ? "opacity-100" : "opacity-0"
+																	)}
+																/>
+															</CommandItem>
+														))}
+													</CommandGroup>
+												</CommandList>
+											</Command>
+										</PopoverContent>
+									</Popover>
+
+									{currentValue && (
+										<Button 
+											size="icon" 
+											type="button"
+											className="ml-0"
+											onClick={() => {
+												setValue(`pairs.${index}.name`, '')
+											}}
 										>
-											{currentValue ? 
-												participants.find(participant => participant.value === currentValue.toLowerCase())?.label : 
-												'Selecione...'}
-											<ChevronsUpDown className="opacity-50" />
+											<Eraser />
 										</Button>
-									</PopoverTrigger>
+									)}
+								</div>
+							</div>
+						)
+					})}
 
-									<PopoverContent className="w-[200px] p-0">
-										<Command>
-											<CommandInput placeholder="Digite o nome..." className="h-9" />
-											<CommandList>
-												<CommandEmpty>Atleta não encontrado.</CommandEmpty>
-
-												<CommandGroup>
-													{participants.map(participant => (
-														<CommandItem
-															key={participant.value}
-															value={participant.value}
-															onSelect={value => {
-																setValue(`pairs.${index}.name`, value)
-															}}
-														>
-															{participant.label}
-
-															<Check
-																className={cn(
-																	"ml-auto",
-																	currentValue === participant.value ? "opacity-100" : "opacity-0"
-																)}
-															/>
-														</CommandItem>
-													))}
-												</CommandGroup>
-											</CommandList>
-										</Command>
-									</PopoverContent>
-								</Popover>
-							)}
-						</div>
-					)
-				})}
-
-				<Button type="submit">Concluir</Button>
-			</form>
+					<Button type="submit" disabled={!isReady} className="justify-self-end col-start-2 w-full">Concluir</Button>
+				</form>
+			)}
 		</div>
 	)
 }
